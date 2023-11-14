3fadb3a196cc6ea51798ee1680d7581e3f5041a4
MPavel02 <maskal0208@mail.ru>
2023-11-14 21:41:04 +0500

    Доработка Dependency Injection

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+using System.Reflection;
 
 namespace Notes.Application
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
     }
 }
