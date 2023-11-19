1efa46cc5850489009a225eb6b1e1131756c968c
MPavel02 <>
2023-11-19 15:46:39 +0500

    Добавлен контроллер аутентификации

---
+++ Notes.Identity/Notes.Identity/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Identity.Models;
+
+namespace Notes.Identity.Controllers
+{
+    public class AuthController
+        : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IIdentityServerInteractionService _interactionService;
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            var viewModel = new LoginViewModel 
+            { 
+                ReturnUrl = returnUrl
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
--- Notes.Identity/Notes.Identity/Startup.cs
+++ Notes.Identity/Notes.Identity/Startup.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Notes.Identity.Data;
 using Notes.Identity.Models;
+using System.IO;
 
 namespace Notes.Identity
 {
@@ -51,6 +52,8 @@ namespace Notes.Identity
                 config.LoginPath = "/Auth/Login";
                 config.LogoutPath = "/Auth/Logout";
             });
+
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -59,15 +62,17 @@ namespace Notes.Identity
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, "Styles")),
+                RequestPath = "/styles"
+            });
             app.UseRouting();
             app.UseIdentityServer();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
